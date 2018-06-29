49947ac8cbf101fed63328ff6630bb7b1b512428
hodiligent <diligentljh@163.com>
2018-06-29 21:24:56 +0800

    第九次作业

--- src/main/java/com/qingguatang/java5minute/course3/SongInfoControl.java
+++ src/main/java/com/qingguatang/java5minute/course3/SongInfoControl.java
@@ -1,6 +1,9 @@
 package com.qingguatang.java5minute.course3;
 
 import com.qingguatang.java5minute.course3.model.SongInfo;
+import com.qingguatang.java5minute.course4.model.Comment;
+import com.qingguatang.java5minute.course5.CommentPostControl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,11 +11,16 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.PostConstruct;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Controller
 public class SongInfoControl {
     private static Map<String,SongInfo> songInfoMap = new HashMap<>();
+
+    @Autowired
+    private CommentPostControl commentPostControl;
+
     @RequestMapping(value = "/songinfo")
     public String index(String songId,ModelMap modelMap){
         if(songId == null){
@@ -20,6 +28,10 @@ public class SongInfoControl {
         }
         SongInfo songInfo = songInfoMap.get(songId);
         modelMap.addAttribute("song",songInfo);
+
+        List<Comment> comments = commentPostControl.getComments(songId);
+
+        modelMap.addAttribute("comments",comments);
         return "index";
     }
 
