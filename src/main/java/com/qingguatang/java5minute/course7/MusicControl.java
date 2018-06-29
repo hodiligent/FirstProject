173bf2fd4d22aeda3a1f20274a8b2e8c77086d56
hodiligent <diligentljh@163.com>
2018-06-29 20:57:33 +0800

    第八次作业

--- src/main/java/com/qingguatang/java5minute/course7/MusicControl.java
+++ src/main/java/com/qingguatang/java5minute/course7/MusicControl.java
@@ -13,7 +13,7 @@ public class MusicControl {
 
     private static final Map<String,String> musics = new HashMap<>();
     @RequestMapping(value = "/song/music")
-    //ResponseBody需要返回
+    //ResponseBody需要返回JSON
     @ResponseBody
     public String getMusicSrc(String songId) {
         return musics.get(songId);
@@ -22,6 +22,9 @@ public class MusicControl {
     @PostConstruct
     public void init(){
         musics.put("35847388",
-                "http://m10.music.126.net/20180615212709/0caf9c838359ee818bfc99736e76e2e1/ymusic/de9a/6885/e183/effb8f74546c98fd619d51f22e2bfaaf.mp3");
+                "http://m10.music.126.net/20180629211754/0f78394ad9529a8ac1cd03f8751f6cbf/ymusic/de9a/6885/e183/effb8f74546c98fd619d51f22e2bfaaf.mp3");
+
+        musics.put("16435049",
+                "http://m10.music.126.net/20180629211914/f6a1c2a2881c6419bac8c5b9b4c88025/ymusic/0cce/0768/52ed/db86bd40d6ba02bc4dc09cc942216662.mp3");
     }
 }
